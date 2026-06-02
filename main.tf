7e69b7226da5ac6bc5bf0277a185ffc554fcd3ec
Ryan Elliott <ryan@ryvn.ai>
2026-06-02 17:38:38 -0400

    test: add apply failure toggle

--- main.tf
+++ main.tf
@@ -21,6 +21,21 @@ variable "delay_url" {
   default = ""
 }
 
+variable "fail_apply" {
+  type    = bool
+  default = false
+}
+
+variable "failure_nonce" {
+  type    = string
+  default = "0"
+}
+
+variable "apply_failure_sleep_seconds" {
+  type    = number
+  default = 30
+}
+
 locals {
   effective_delay_url = var.delay_url != "" ? var.delay_url : "http://host.docker.internal:8089/?seconds=${var.sleep_seconds}"
 }
@@ -42,6 +57,23 @@ resource "terraform_data" "marker" {
   input = {
     plan_body_hash = sha256(data.http.slow_plan.response_body)
     delay_id       = time_sleep.operation_delay.id
+  }
+}
+
+resource "terraform_data" "apply_failure" {
+  input = {
+    delay_id = time_sleep.operation_delay.id
+  }
+
+  triggers_replace = {
+    fail_apply    = var.fail_apply
+    failure_nonce = var.failure_nonce
+  }
+
+  provisioner "local-exec" {
+    when        = create
+    interpreter = ["/bin/sh", "-c"]
+    command     = var.fail_apply ? "sleep ${var.apply_failure_sleep_seconds}; echo forced apply failure >&2; exit 42" : "true"
   }
 }
 
